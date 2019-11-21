e88af99f5a2eddc03adb86fa3651f1f2af6d9f22
SW <jclopes99@gmail.com>
2019-11-21 15:25:57 +0000

    basic packet send/receive working

--- common.h
+++ common.h
@@ -1,5 +1,10 @@
 /* Yo Yo Yo */
 
+#define LOCALHOST "127.0.0.1" /* Using this for ease of debugging*/
+#define MAXSIZE 1000
+#define TRUE 1999
+#define MYPORT 6969
+
 void createSocket(int port);
 
 void sendPacket(char *payload);
