b0bc9bc9e839da8ab440373e861d4240e1f63ce0
Hugo <hugo.martins365@gmail.com>
2019-11-29 10:47:44 +0000

    passar ao pedro

--- Receiver/receiver.c
+++ Receiver/receiver.c
@@ -7,14 +7,15 @@
 #include <netdb.h>
 #include <sys/socket.h>
 
-char *filename;
+
 int port, windowsize;
 
 int main(int argc, char const *argv[])
 {
-    int n, base, raw_socket, writeLength;
-    uint32_t mask;
-    FILE *fp;
+    int n, raw_socket, writeLength = 1000;
+    uint32_t mask = 0;
+    uint32_t base = 1;
+        FILE *fp;
     struct sockaddr_in servaddr;
     data_pkt_t data;
     ack_pkt_t packet;
@@ -24,11 +25,11 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "main(): too few args!");
         exit(-1);
     }
-    filename = argv[1];
-    port = argv[2];
-    windowsize = argv[3];
+    port = atoi(argv[2]);
+    windowsize = atoi(argv[3]);
 
-    if ((raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) {
+    if ((raw_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
         fprintf(stderr, "main(): socket error!");
         exit(-1);
     }
@@ -45,7 +46,7 @@ int main(int argc, char const *argv[])
     }
 
     base = 1;
-    fp = fopen(filename, "w");
+    fp = fopen(argv[1], "w");
 
     if (fp == NULL) {
         perror("main(): fopen error!");
@@ -56,50 +57,52 @@ int main(int argc, char const *argv[])
     socklen_t len = sizeof(servaddr);
     while (1)
     {
-
-        if (writeLength == sizeof(data.data))
+        if ((n = recvfrom(raw_socket, &data, MAXSIZE, 0, (struct sockaddr *)&servaddr, &len)) < 1)
         {
-            
-            if ((n = recvfrom(raw_socket, &data, MAXSIZE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len)) < 1) {
-                fprintf(stderr, "main(): recvfrom error!");
-                fclose(fp);
-                close(raw_socket);
-                exit(-1);
-            }
+            fprintf(stderr, "main(): recvfrom error!");
+            fclose(fp);
+            close(raw_socket);
+            exit(-1);
+        }
 
-            fprintf(stdout, "Received: %s\n", data.data);
-            if (data.seq_num > base + windowsize) {
+        if (writeLength == MAXSIZE)
+        {
+            data.seq_num = htonl(data.seq_num);
+            fprintf(stdout, "Received: %d\n", data.seq_num);
+            if (data.seq_num > base + windowsize)
+            {
                 /*drop packet*/
                 continue;
             }
-
             if (data.seq_num < base) {
                 /*This means it was already acknowledged*/
-                packet.seq_num = base;
-                packet.selective_acks = mask;
+                packet.seq_num = ntohl(base);
+                packet.selective_acks = ntohl(mask);
             }
-
-            if (data.seq_num == base) {
+            else if (data.seq_num == base) {
                 /*shifts the base, sends ack*/
                 while (mask << 31 != 0) {
+                    printf("hello\n");
                     base = base + 1;
                     mask = mask >> 1;
                 }
+                printf("~hi\n");
                 base = base + 1;
                 mask = mask >> 1;
-                packet.seq_num = base;
-                packet.selective_acks = mask;
+                packet.seq_num = ntohl(base);
+                packet.selective_acks = ntohl(mask);
+                printf("pila");
             }
 
-            if (data.seq_num > base && data.seq_num < base + windowsize) {
+            else if (data.seq_num > base && data.seq_num < base + windowsize) {
                 /* if it's in the middle of the window, the base stays the same, the selectiveAck is changed*/
                 int s = data.seq_num - base;
                 mask = mask | (1 >> s);
-                packet.seq_num = data.seq_num;
-                packet.selective_acks = mask;
+                packet.seq_num = ntohl(data.seq_num);
+                packet.selective_acks = ntohl(mask);
             }
-
-            if (sendto(raw_socket, (void *)&packet, sizeof(packet), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+            if (sendto(raw_socket, (void *)&packet, sizeof(packet), 0, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+            {
                 perror("Send to");
                 close(raw_socket);
                 fclose(fp);
